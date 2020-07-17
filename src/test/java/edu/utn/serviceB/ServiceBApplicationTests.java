4bdda86fc70be66d72002f800ac2dc98639ebc90
Fabio Laguna <fabiolaguna.94@gmail.com>
2020-07-17 18:33:51 -0300

    Commit inicial

---
+++ src/test/java/edu/utn/serviceB/ServiceBApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.utn.serviceB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
